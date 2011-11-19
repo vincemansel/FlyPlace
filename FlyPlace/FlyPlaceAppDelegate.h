84843918cf39477fc7205507474da65a208e0d59
Vince Mansel <vincemansel@gmail.com>
2011-11-19 14:56:52 -0800

    Initial Commit

---
+++ FlyPlace/FlyPlaceAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FlyPlaceAppDelegate.h
+//  FlyPlace
+//
+//  Created by Vince Mansel on 11/19/11.
+//  Copyright (c) 2011 Wave Ocean Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlyPlaceAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FlyPlace/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FlyPlace
+//
+//  Created by Vince Mansel on 11/19/11.
+//  Copyright (c) 2011 Wave Ocean Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FlyPlaceAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FlyPlaceAppDelegate class]));
+    }
+}
